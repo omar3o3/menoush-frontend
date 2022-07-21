4ca3c9be72bbb70ef215a0519ed52b18acc4b2f4
omar3o3 <otantawy53@gmail.com>
2022-07-21 16:35:57 -0400

    getting ready to create landing page/home

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function App() {
+
+
+
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
