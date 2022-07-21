e7d474fa4e74286412400512b31ca2390ff179e7
omar3o3 <otantawy53@gmail.com>
2022-07-21 17:19:03 -0400

    adding routes and react bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import UserHome from "./UserHome"
+import UserAccount from "./UserAccount"
 
 function App() {
 
+  let somefunc = () => {
 
+  }
 
   return (
     <>
+      <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <UserHome/>
+            </Route>
+
+            <Route path="/user-account">
+              <UserAccount/>
+            </Route>
+        </Switch>
+      </BrowserRouter>
     </>
   );
 }
