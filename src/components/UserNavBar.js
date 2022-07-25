7942d07331c586f24363085ff7c1853299ec3983
omar3o3 <otantawy53@gmail.com>
2022-07-25 11:45:13 -0400

    both users and admins can login and logout

--- src/components/UserNavBar.js
+++ src/components/UserNavBar.js
@@ -5,21 +5,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
-function UserNavBar() {
+function UserNavBar({user , setUser}) {
+
+  function handleLogoutClick() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+
   return (
     <Navbar bg="dark" variant="dark">
-        <Container className="fs-3">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/gallery">Gallery</Nav.Link>
-            <Nav.Link href="/cart">Cart</Nav.Link>
-            <Nav.Link href="/userProfile">*icon*</Nav.Link>
-            {/* <Nav.Link href="/logout">Logout</Nav.Link> */}
-          </Nav>
-            <Button>Logout</Button>
-        </Container>
+      <Container className="fs-3">
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/gallery">Gallery</Nav.Link>
+          <Nav.Link href="/cart">Cart</Nav.Link>
+          <Nav.Link href="/userProfile">*icon*</Nav.Link>
+          {/* <Nav.Link href="/logout">Logout</Nav.Link> */}
+        </Nav>
+        <Button onClick={handleLogoutClick}>Logout</Button>
+      </Container>
     </Navbar>
-  )
+  );
 }
 
 export default UserNavBar
