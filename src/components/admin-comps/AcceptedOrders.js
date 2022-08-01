cdffb1247223e9a05925c88e4850605d47d88b89
omar3o3 <otantawy53@gmail.com>
2022-08-01 13:46:06 -0400

    done with this branch

--- src/components/admin-comps/AcceptedOrders.js
+++ src/components/admin-comps/AcceptedOrders.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React , {useState , useEffect} from 'react'
+import RenderAcceptedOrders from "./RenderAcceptedOrders";
 
 function AcceptedOrders() {
+  const [acceptedOrdersState , setAcceptedOrdersState] = useState()
+
+  useEffect(() => {
+    fetch("/get-accepted-orders")
+      .then((resp) => resp.json())
+      .then((data) => setAcceptedOrdersState(data));
+  }, [])
+
+  console.log(acceptedOrdersState);
+
   return (
-    <div>AcceptedOrders</div>
-  )
+    <div className="pendingOrderBottomMargin">
+      {acceptedOrdersState && acceptedOrdersState.length !== 0 ? (
+        acceptedOrdersState.map((order) => (
+          <RenderAcceptedOrders
+            key={order.id}
+            order={order}
+            cartItems={order.cart_items}
+            desserts={order.desserts}
+            UserFullName={order.show_pending_cart_name}
+          />
+        ))
+      ) : (
+        <h1 className="fs-1 text-center">No Accepted Orders</h1>
+      )}
+    </div>
+  );
 }
 
 export default AcceptedOrders
---
+++ src/components/admin-comps/RenderAcceptedOrders.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+
+function RenderAcceptedOrders({ order, cartItems, desserts, UserFullName }) {
+  const [showState, setShowState] = useState(true);
+
+  let totalPrice = cartItems
+    .map((item) => parseFloat(item.self_total))
+    .reduce((partialSum, a) => partialSum + a, 0);
+
+  let handleCompleted = () => {
+    console.log(order);
+    fetch("/complete-order", {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        order_id: order.id,
+      }),
+    }).then((response) => {
+      if (response.ok) {
+        setShowState(false);
+      }
+    });
+  };
+
+  return (
+    <>
+      {showState ? (
+        <Container
+          className="border border-2 border-dark rounded individalPendingOrderMargin"
+          style={{ width: "70%" }}
+        >
+          <Row
+            style={{ backgroundColor: "#FFCDD2" }}
+            className="fs-3 fw-light text-center"
+          >
+            <Col className="border-end border-2 border-dark">
+              <div>Name: {UserFullName}</div>
+            </Col>
+            {/* <Col className="border-end border-dark">
+            <div>Date Ordered: {}</div>
+          </Col> */}
+            <Col className="border-end border-2 border-dark">
+              <div>Complete with: {order.days_to_complete} days</div>
+            </Col>
+            <Col className="">
+              <div>Total: ${totalPrice}</div>
+            </Col>
+          </Row>
+
+          <Row className="fs-3 fw-light">
+            <Col className="border-top border-dark border-bottom text-center">
+              Items...
+            </Col>
+          </Row>
+
+          <Row>
+            <Col className="">
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <p className="lead">
+                    {desserts.map((dessert) => (
+                      <li key={dessert.id}>
+                        <span className="fw-bold">
+                          {
+                            cartItems.find(
+                              (item) => item.dessert_id === dessert.id
+                            ).quantity
+                          }
+                          &nbsp;&nbsp;
+                        </span>
+                        {dessert.english_name}
+                        &nbsp;&nbsp; / &nbsp;&nbsp;
+                        {dessert.arabic_name}
+                      </li>
+                    ))}
+                  </p>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+
+          <Row className="border-top border-dark">
+            <Button variant="success" onClick={() => handleCompleted()}>
+              Complete Order
+            </Button>
+          </Row>
+        </Container>
+      ) : null}
+    </>
+  );
+}
+
+export default RenderAcceptedOrders;
