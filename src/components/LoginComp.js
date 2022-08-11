1cf527b8863da1aa06fcec4cc1918f2755c1571c
omar3o3 <otantawy53@gmail.com>
2022-08-11 18:06:14 -0400

    finished some login css

--- src/components/LoginComp.js
+++ src/components/LoginComp.js
@@ -1,15 +1,25 @@
-import React , {useState} from 'react'
+import React, { useState } from "react";
 
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 import menoushBlackLogo from "../images/menoushBlackLogo.png";
 
-import LoginForm from './LoginForm';
-import SignUpForm from './SignUpForm';
+import LoginForm from "./LoginForm";
+import SignUpForm from "./SignUpForm";
 
-function LoginComp({onLogin}) {
-    
-    const [showLogin, setShowLogin] = useState(true);
+function LoginComp({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(true);
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
 
   return (
     <div
@@ -45,24 +55,57 @@ function LoginComp({onLogin}) {
           <>
             <LoginForm onLogin={onLogin} />
             <br />
-            <p style={{ color: "#d8a941", textAlign: "center" }}>
-              Don't have an account? &nbsp;
-              <Button color="secondary" onClick={() => setShowLogin(false)}>
-                Sign Up
-              </Button>
+            <p
+              // className="mx-0 px-0"
+              style={{
+                color: "#d8a941",
+                textAlign: "center",
+                display: "inline",
+              }}
+            >
+              Don't have an account? &nbsp; &nbsp;
             </p>
+            <Button
+              onClick={() => setShowLogin(false)}
+              className="border-1 border-dark"
+              style={{
+                backgroundColor: isHovering ? "#1d1a0c" : "white",
+                color: isHovering ? "white" : "black",
+                display: "inline",
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              Sign Up
+            </Button>
           </>
         ) : (
           <>
             <SignUpForm onLogin={onLogin} />
             <br />
 
-            <p style={{ textAlign: "center", color: "#d8a941" }}>
-              Already have an account? &nbsp;
-              <Button color="secondary" onClick={() => setShowLogin(true)}>
-                Log In
-              </Button>
+            <p
+              style={{
+                textAlign: "center",
+                color: "#d8a941",
+                display: "inline",
+              }}
+            >
+              Already have an account? &nbsp; &nbsp;
             </p>
+            <Button
+              onClick={() => setShowLogin(true)}
+              className="border-1 border-dark"
+              style={{
+                backgroundColor: isHovering ? "#1d1a0c" : "white",
+                color: isHovering ? "white" : "black",
+                display: "inline",
+              }}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              Log In
+            </Button>
           </>
         )}
       </div>
@@ -70,4 +113,4 @@ function LoginComp({onLogin}) {
   );
 }
 
-export default LoginComp
+export default LoginComp;
